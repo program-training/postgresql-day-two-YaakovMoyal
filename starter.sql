0e1b5d4e1b927a01d600d811bad6e2f0abff60a3
YaakovMoyal <kobi856m@gmail.com>
2023-09-13 16:42:45 +0300

    part C

--- starter.sql
+++ starter.sql
@@ -8,16 +8,16 @@ CREATE TABLE StudyGroup(
 INSERT INTO StudyGroup (name_group, student_id)
 VALUES('group 1',1), ('group 2',2)
 
-SELECT * FROM courses
+SELECT * FROM courses;
 
-SELECT * FROM enrollments
+SELECT * FROM enrollments;
 
-SELECT * FROM grades
+SELECT * FROM grades;
 
-SELECT * FROM students
+SELECT * FROM students;
 
-SELECT * FROM StudyGroup
+SELECT * FROM StudyGroup;
 
-SELECT * FROM TeacherCourse
+SELECT * FROM TeacherCourse;
 
-SELECT * FROM teachers
+SELECT * FROM teachers;
